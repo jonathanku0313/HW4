945624cfdf141295f212fe24aa7d39433dba9c69
jonathanku0313 <61576551+jonathanku0313@users.noreply.github.com>
2020-06-07 23:04:43 +0800

    Origin

--- HW4/mqtt_client.py
+++ HW4/mqtt_client.py
@@ -9,9 +9,8 @@ host = "localhost"
 topic= "Mbed"
 port = 1883
 
-t = np.arange(0,20,1)
-tilt = []
-temp = 1
+t = np.arange(0,18,1)
+tilt = np.arange(0,18,1)
 i = 0
 
 # Callbacks
@@ -19,9 +18,12 @@ def on_connect(self, mosq, obj, rc):
     print("Connected rc: " + str(rc))
 
 def on_message(mosq, obj, msg):
+    global i
     print("[Received] Topic: " + msg.topic + ", Message: " + str(msg.payload) + "\n")
     print(float(msg.payload))
-    tilt.append(float(msg.payload))
+    print(i)
+    tilt[i] = float(msg.payload)
+    i += 1
 
 def on_subscribe(mosq, obj, mid, granted_qos):
     print("Subscribed OK")
@@ -40,17 +42,15 @@ print("Connecting to " + host + "/" + topic)
 mqttc.connect(host, port=1883, keepalive=60)
 mqttc.subscribe(topic, 0)
 
-while i <= 20:
+while True:
     mqttc.loop()
-    time.sleep(1)
-    i+=1
-
-if i > 20:
-    plt.figure()
-    plt.stem(t,tilt)
-    plt.xlabel('Time')
-    plt.ylabel('Tilt')
-    plt.show()
+    if i >= 18:
+        plt.figure()
+        plt.stem(t,tilt)
+        plt.xlabel('Time')
+        plt.ylabel('Tilt')
+        plt.show()
+        break
 
 #Loop forever, receiving messages
 #mqttc.loop_forever()
